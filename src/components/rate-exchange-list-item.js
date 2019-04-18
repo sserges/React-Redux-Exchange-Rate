f593f0481dc9c9903f28989139d72c2a6125c2e1
sserges <agounkeserge@gmail.com>
2019-04-18 15:45:11 +0000

    manage charts with chartkick

--- src/components/rate-exchange-list-item.js
+++ src/components/rate-exchange-list-item.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
+import ReactChartKick, { AreaChart } from "react-chartkick";
+import Chart from "chart.js";
+
+ReactChartKick.addAdapter(Chart);
 
 export class RateExchangeListItem extends Component {
   render() {
-    const { flag, name, rates } = this.props.rateExchange;
+    const { flag, name, rates, currencyCode } = this.props.rateExchange;
     return (
       <tr>
         <td>
           {name} <br />{" "}
           <img alt={`${name}-flag`} height="60" width="100" src={flag} />
         </td>
-        <td>Graphique</td>
+        <td className="col-md-12">
+          <AreaChart
+            data={formatData(rates, currencyCode)}
+            xtitle="Date"
+            ytitle={currencyCode}
+          />
+        </td>
       </tr>
     );
   }
+}
+
+function formatData(rates, currencyCode) {
+  return Object.keys(rates).map(date => {
+    return [date, rates[date][currencyCode]];
+  });
 }
 
 export default RateExchangeListItem;
