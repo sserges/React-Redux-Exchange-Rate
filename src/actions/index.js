9bf3e051221fe2cd53940e06dc334d18c1dc74f2
sserges <agounkeserge@gmail.com>
2019-04-18 11:19:32 +0000

    manage the start and end dates

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const GET_COUNTRIES = "GET_COUNTRIES";
+export const GET_RATE_EXCHANGE = "GET_RATE_EXCHANGE";
 
 export function fetchCountries() {
   return function(dispatch) {
@@ -8,4 +9,31 @@ export function fetchCountries() {
       dispatch({ type: GET_COUNTRIES, payload: axiosResponse.data });
     });
   };
+}
+
+export function fetchRateExchange(country) {
+  return function(dispatch) {
+    axios
+      .get(
+        `https://api.exchangeratesapi.io/history?start_at=${getLastMonth()}&end_at=${formatedDate(
+          new Date()
+        )}&base=USD&symbols=${country.currencyCode}`
+      )
+      .then(axiosResponse => {
+        dispatch({
+          type: GET_RATE_EXCHANGE,
+          payload: { rates: axiosResponse.data.rates, ...country }
+        });
+      });
+  };
+}
+
+function formatedDate(date) {
+  return date.toISOString().split("T")[0];
+}
+
+function getLastMonth() {
+  let now = new Date();
+  now.setMonth(now.getMonth() - 1);
+  return formatedDate(now);
 }
